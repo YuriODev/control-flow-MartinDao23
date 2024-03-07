b0f21ccd4d0f4b6c81cd67c60154930df5d3f340
MartinDao23 <161600728+MartinDao23@users.noreply.github.com>
2024-03-07 10:27:44 +0000

    exercise 3

--- exercises/exercise_3.py
+++ exercises/exercise_3.py
@@ -1,2 +1,13 @@
-# Your solution to Exercise 3
-
+number = int(input("Enter a number between 0 and 36:   "))
+colour = ""
+if number == 0:
+    colour = "green"
+elif number <= 10:
+    colour = "red" if number%2 == 1 else "black"
+elif number <= 18:
+    colour = "black" if number%2 == 1 else "red"
+elif number <= 28:
+    colour = "red" if number%2 == 1 else "black"
+else:
+    colour = "black" if number%2 == 1 else "red"
+print(colour)
