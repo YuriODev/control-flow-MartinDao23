68b031f03a4e4736f517e2cd87012b5a8bf34a8e
MartinDao23 <161600728+MartinDao23@users.noreply.github.com>
2024-03-07 10:37:42 +0000

    exercise 4

--- exercises/exercise_4.py
+++ exercises/exercise_4.py
@@ -1,2 +1,13 @@
-# Your solution to Exercise 4
-
+grade = int(input("Enter the grade:   "))
+output = ""
+if 1 > grade > 9:
+    output = "level is absent"
+elif grade <= 3:
+    output = "initial level"
+elif grade <= 6:
+    output = "average level"
+elif grade <= 9:
+    output = "sufficient level"
+else:
+    output = "high level"
+print(output)
