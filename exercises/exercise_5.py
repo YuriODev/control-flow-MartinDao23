c3d44c99f2505824b6c5910ab72ba13065882898
MartinDao23 <161600728+MartinDao23@users.noreply.github.com>
2024-03-07 10:53:07 +0000

    exercise 5

--- exercises/exercise_5.py
+++ exercises/exercise_5.py
@@ -1,2 +1,12 @@
-# Your solution to Exercise 5
-
+a = int(input("Enter A  "))
+b = int(input("Enter B  "))
+c = int(input("Enter C  "))
+if (b**2-4*a*c) < 0:
+    print("ro real roots")
+elif (b**2-4*a*c) == 0:
+    root1 = (-b+(b**2-4*a*c)**0.5)/(2*a)
+    print(root1)
+else:
+    root1 = (-b+(b**2-4*a*c)**0.5)/(2*a)
+    root2 = (-b-(b**2-4*a*c)**0.5)/(2*a)
+    print(f"{root1} and {root2}")
